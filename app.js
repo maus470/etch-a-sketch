1bdf75a2732154765c36cd5efcf148e0064a9628
maus470 <alvarowindows007@gmail.com>
2026-06-28 21:04:40 +0200

    added random rgb color and darkening effect

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+function random(num) {
+    return Math.floor(Math.random() * (num + 1));
+}
+
+function createGrid() {
+    for (let i = 0; i < tiles; i++) {
+        let row = document.createElement("div");
+        row.style.display = "flex";
+        row.style.flexDirection = "row";
+        for (let j = 0; j < tiles; j++) {
+            let item = document.createElement("div");
+            item.classList.add("tile");
+            item.style.height = `${tileSize}px`;
+            item.style.width = `${tileSize}px`;
+            item.style.backgroundColor = `rgb(240, 240, 240)`;
+            item.addEventListener("mouseenter", () => {
+                if (!item.classList.contains("hover")) {
+                    item.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+                    item.classList.add("hover");
+                    item.style.opacity = 1;
+                } else {
+                    // let opacity = item.style.
+                    item.style.opacity -= 0.1;
+                }
+            });
+            row.appendChild(item);
+        }
+        container.appendChild(row);
+    }
+}
+
 let container = document.querySelector(".container");
 let tiles = 16;
 const dimensions = 960;
@@ -22,22 +53,3 @@ button.addEventListener("click", () => {
     createGrid();
     text.textContent = `${tiles}x${tiles}`;
 });
-
-function createGrid() {
-    for (let i = 0; i < tiles; i++) {
-        let row = document.createElement("div");
-        row.style.display = "flex";
-        row.style.flexDirection = "row";
-        for (let j = 0; j < tiles; j++) {
-            let item = document.createElement("div");
-            item.classList.add("tile");
-            item.style.height = `${tileSize}px`;
-            item.style.width = `${tileSize}px`;
-            item.addEventListener("mouseenter", () => {
-                item.classList.add("hover");
-            });
-            row.appendChild(item);
-        }
-        container.appendChild(row);
-    }
-}
